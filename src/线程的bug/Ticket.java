801d5ba40ab0b8b0c983bacb46e778762b1465c4
yanbo0039 <yanbo0039@yeah.net>
2019-07-17 17:14:15 +0800

    解决bug的方法三是要Lock对象

--- src/线程的bug/Ticket.java
+++ src/线程的bug/Ticket.java
@@ -1,15 +1,44 @@
 package 线程的bug;
 
+//import java.util.concurrent.locks.Lock;//解决bug方法三：使用Lock对象
+//import java.util.concurrent.locks.ReentrantLock;//解决bug方法三：使用Lock对象
+
 public class Ticket {
 
 	private int count = 1000;
 	
-	public synchronized void saleTicket() {//解决bug方法一：上锁 加关键字synchronized
-//		synchronized (this) {//解决bug方法二：锁住共享对象
-			if(count > 0) {
-				System.out.println(Thread.currentThread().getName() + ": " + count);
-				count--;
-			}
+//	private Lock lock = new ReentrantLock();//解决bug方法三：使用Lock对象
+	
+//	public synchronized void saleTicket() {//解决bug方法一：上锁 加关键字synchronized
+//		if(count > 0) {
+//			System.out.println(Thread.currentThread().getName() + ": " + count);
+//			count--;
 //		}
+//	}
+	
+//	public void saleTicket() {
+//		synchronized (this) {//解决bug方法二：锁住共享对象
+//			if(count > 0) {
+//				System.out.println(Thread.currentThread().getName() + ": " + count);
+//				count--;
+//			}
+//		}
+//	}
+	
+//	public void saleTicket() {//解决bug方法三：使用Lock对象
+//		lock.lock();//上锁
+//		if(count > 0) {
+//			System.out.println(Thread.currentThread().getName() + ": " + count);
+//			count--;
+//		}
+//		lock.unlock();//解锁
+//	}
+	
+	//产生bug
+	public void saleTicket() {
+		if(count > 0) {
+			System.out.println(Thread.currentThread().getName() + ": " + count);
+			count--;
+		}
 	}
 }
